c2d6837a8c755dd362d4b74edd8959dfd7c1920c
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-10-22 13:19:40 -0300

    Endpoints to follow and unfollow using POST method

--- djangoapp/users/views.py
+++ djangoapp/users/views.py
@@ -27,56 +27,76 @@ class UserViewSet(ModelViewSet):
         return User.objects.filter(pk=self.request.user.pk)
 
     def get_permissions(self):
-        if self.request.method == "POST":
+        if self.request.method == "POST" and not (
+            "follow/" in self.request.path or "unfollow" in self.request.path
+        ):
             return [CanCreate()]
         return super().get_permissions()
 
-
     @action(
-        methods=['get'],
+        methods=["POST"],
         detail=False,
-        url_path='follow/(?P<id_user_to_follow>[^/.]+)'
+        url_path="follow",
+        permission_classes=[IsAuthenticated],
     )
     def follow(self, request, *args, **kwargs):
         User = get_user_model()
-        
+
         user_following = request.user
-        
-        id_user_to_follow = kwargs.get('id_user_to_follow')
-        user_to_follow = get_object_or_404(User, id = id_user_to_follow)
+
+        id_user_to_follow = request.data.get("id_user_to_follow")
+
+        if not id_user_to_follow:
+            raise ValidationError({"error": "User ID(id_user_to_follow) is required."})
+
+        user_to_follow = get_object_or_404(User, id=id_user_to_follow)
 
         # User can not follow who his already follow
-        if user_following.profile.following.filter(pk=user_to_follow.profile.pk).exists(): # type: ignore
-            raise ValidationError({'detail': 'You are already following this user.'})
-        
+        if user_following.profile.following.filter(
+            pk=user_to_follow.profile.pk  # type: ignore
+        ).exists():
+            raise ValidationError({"detail": "You are already following this user."})
+
         # User can not follow itself
         if user_following == user_to_follow:
-            raise ValidationError({'detail': 'You can not follow yourself.'})
-        
+            raise ValidationError({"detail": "You can not follow yourself."})
+
         # User can`t follow an admin user
         if user_to_follow.is_staff:
-            raise ValidationError({'detail': 'You can not follow this user.'})
-        
-        user_following.profile.following.add(user_to_follow.profile) # type: ignore
-        user_to_follow.profile.followers.add(user_following.profile) # type: ignore
-        return Response({'detail': 'Successfully followed the user.'}, status=status.HTTP_204_NO_CONTENT)
+            raise ValidationError({"detail": "You can not follow this user."})
+
+        user_following.profile.following.add(user_to_follow.profile)  # type: ignore
+        user_to_follow.profile.followers.add(user_following.profile)  # type: ignore
+        return Response(
+            {"detail": "Successfully followed the user."},
+            status=status.HTTP_204_NO_CONTENT,
+        )
 
     @action(
-        methods=['get'],
+        methods=["POST"],
         detail=False,
-        url_path='unfollow/(?P<id_user_to_unfollow>[^/.]+)'
+        url_path="unfollow",
     )
     def unfollow(self, request, *args, **kwargs):
         User = get_user_model()
-        
-        user_unfollowing = request.user
-        
-        id_user_to_unfollow = kwargs.get('id_user_to_unfollow')
-        user_to_unfollow = get_object_or_404(User, id = id_user_to_unfollow)
 
-        if user_unfollowing.profile.following.filter(pk=user_to_unfollow.profile.pk).exists(): # type: ignore
-            user_unfollowing.profile.following.remove(user_to_unfollow.profile) # type: ignore
-            user_to_unfollow.profile.followers.remove(user_unfollowing.profile) # type: ignore
-            return Response({'detail': 'Successfully unfollowed the user.'}, status=status.HTTP_204_NO_CONTENT)
-        raise ValidationError({'detail': 'You are not following this user.'})
-        
+        user_unfollowing = request.user
+
+        id_user_to_unfollow = request.data.get("id_user_to_unfollow")
+        if not id_user_to_unfollow:
+            raise ValidationError(
+                {"error": "User ID(id_user_to_unfollow) is required."}
+            )
+
+        user_to_unfollow = get_object_or_404(User, id=id_user_to_unfollow)
+
+        if user_unfollowing.profile.following.filter(
+            pk=user_to_unfollow.profile.pk  # type: ignore
+        ).exists():
+            user_unfollowing.profile.following.remove(user_to_unfollow.profile)  # type: ignore
+            user_to_unfollow.profile.followers.remove(user_unfollowing.profile)  # type: ignore
+            return Response(
+                {"detail": "Successfully unfollowed the user."},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        raise ValidationError({"detail": "You are not following this user."})
